c11f488e7a11ad6f81259c18fc78b981bc1313d1
paulDev <paul_d18@hotmail.com>
2022-09-24 05:32:19 -0300

    corrigiendo rutas

--- Frontend/ProyectoGDB/src/app/app.module.ts
+++ Frontend/ProyectoGDB/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Libros } from './libros/libros.component';
+import { Libros } from './pages/libros/libros.component';
 
 @NgModule({
   declarations: [
