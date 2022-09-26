3ffa9e34f37be9a30e7715d70634de33f77c5a3b
SalvadorCasas <salvadorcasas11@gmail.com>
2022-09-26 11:18:36 -0300

    agrego bootstrap y arreglos seccion login

--- Frontend/ProyectoGDB/src/app/app-routing.module.ts
+++ Frontend/ProyectoGDB/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IngresarComponent } from './pages/ingresar/ingresar.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { LibrosComponent } from './pages/libros/libros.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /*SI EL USER INTENTA ACCEDER A UNA RUTA Y NO ESPECIFICA NADA LO DIRIGIMOS A INICIO*/
+{path: '', component:InicioComponent},
+{path: 'inicio', component:InicioComponent},
+{path: 'libros', component:LibrosComponent},
+{path: 'registro', component:RegistroComponent},
+{path: 'ingresar', component:IngresarComponent},
+/*SI EL USER INTENTA ACCEDER A UNA RUTA QUE NO HACE MATCH CON NADA LO DIRIGIMOS A REGISTRO*/
+{path:'**', redirectTo: 'registro', pathMatch:'full'},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
