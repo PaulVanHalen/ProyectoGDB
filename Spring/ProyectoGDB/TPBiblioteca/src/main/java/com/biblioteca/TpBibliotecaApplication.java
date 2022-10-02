9341525a77915c509ac47f03b7dbb664c84c242e
paulDev <paul_d18@hotmail.com>
2022-10-02 01:02:22 -0300

    carpeta spring

---
+++ Spring/ProyectoGDB/TPBiblioteca/src/main/java/com/biblioteca/TpBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpBibliotecaApplication.class, args);
+	}
+
+}
